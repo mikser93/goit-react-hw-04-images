c5a1b5dd1a31b7e8170e07459a2dea01ee35d3e0
Vova Vykhrystenko <mikserwowa@gmail.com>
2022-10-31 20:23:47 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,6 @@ import { requesterAPI } from '../services/requesterAPI';
 import noFound from '../images/no-found.png';
 import styles from './App.module.css';
 
-
 function App() {
   const [searchWord, setSearchWord] = useState('');
   const [page, setPage] = useState(1);
@@ -23,52 +22,65 @@ function App() {
     // eslint-disable-next-line
   }, [page, searchWord]);
 
-  const onSubmit = (event) => {
+  const onSubmit = event => {
     event.preventDefault();
     setSearchWord(event.target[1].value);
     setPage(1);
+    setImages([]);
     setIsEndOfGallery(false);
     event.target.reset();
   };
 
   const pageOperator = () => {
-    setPage(prev => prev + 1)
+    setPage(prev => prev + 1);
   };
 
   const getPhotos = async () => {
-    setImages([]);
     setIsLoading(true);
     try {
-      requesterAPI(searchWord, page)
-        .then(response => {
-          setIsEndOfGallery(images.length + response.hits.length === response.totalHits);
-          setImages(prev => [...prev, ...response.hits]);
-        });
+      requesterAPI(searchWord, page).then(response => {
+        setIsEndOfGallery(
+          images.length + response.hits.length === response.totalHits
+        );
+        setImages(prev => [...prev, ...response.hits]);
+      });
     } catch (error) {
       console.log(error);
     } finally {
       setIsLoading(false);
-    };
+    }
   };
 
   const toggleModal = event => {
-    if (event.target === event.currentTarget || event.code ) {
-      const imgPath = event.target.id ? images[event.target.id].largeImageURL : '';
+    if (event.target === event.currentTarget || event.code) {
+      const imgPath = event.target.id
+        ? images[event.target.id].largeImageURL
+        : '';
       setIsModalOpen(prev => !prev);
       setLargeImage(imgPath);
-    };
+    }
   };
 
   return (
     <div className={styles.app}>
       <Searchbar onSubmit={onSubmit} />
-      {images.length > 0 && <ImageGallery images={images} toggleModal={toggleModal} />}
-      {!images.length > 0 && <img src={noFound} alt="depiction no found" style={{margin: "auto", maxWidth: "600px"}} />}
-      {images.length > 0 && !isEndOfGallery && <Button pageOperator={pageOperator} />}
+      {images.length > 0 && (
+        <ImageGallery images={images} toggleModal={toggleModal} />
+      )}
+      {!images.length > 0 && (
+        <img
+          src={noFound}
+          alt="depiction no found"
+          style={{ margin: 'auto', maxWidth: '600px' }}
+        />
+      )}
+      {images.length > 0 && !isEndOfGallery && (
+        <Button pageOperator={pageOperator} />
+      )}
       {isModalOpen && <Modal image={largeImage} toggleModal={toggleModal} />}
       {isLoading && <Loader />}
     </div>
   );
-};
+}
 
 export default App;
--- src/services/requesterAPI.js
+++ src/services/requesterAPI.js
@@ -1,6 +1,6 @@
-export function requesterAPI(serachWord, page) {
+export function requesterAPI(searchWord, page) {
     const API_KEY = '29360709-e61ed8eebe570e88773d53140';
-    const requestPath = `https://pixabay.com/api/?q=${serachWord}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
+    const requestPath = `https://pixabay.com/api/?q=${searchWord}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
 
     return fetch(requestPath)
         .then(resp => {
